c9436f6f0e86367f26739306dac18e7609b47a0f
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-07 18:22:10 -0400

    Added comment

--- oraklesky/src/day.jsx
+++ oraklesky/src/day.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { getDateFromString } from './config';
 
 
-
+// Componenet to display the weather for a single day
 const Day = ({dayInfo}) => {
     console.log(dayInfo)
-    let date = new Date(dayInfo.datetime);
     return (
         <div className="day">
             <h1>{date}</h1>
