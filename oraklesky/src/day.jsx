e39209e7a7c277733c55b15b87aaa5085ddc529c
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-08 05:14:41 -0400

    semantic changes

--- oraklesky/src/day.jsx
+++ oraklesky/src/day.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { getDateFromString } from './config';
 
 
 // Componenet to display the weather for a single day
 const Day = ({dayInfo}) => {
-   console.log(dayInfo)
    const date = dayInfo.datetime
    const description = dayInfo.description
     return (
@@ -13,8 +11,8 @@ const Day = ({dayInfo}) => {
                 <h1>{date}</h1>
             </div>
             <div className='temps'>
-                <h2>Temp: {dayInfo.temp}</h2>
-                <h2>Feels Like: {dayInfo.feelslike}</h2>
+                <p>Temp: {dayInfo.temp}</p>
+                <p>Feels Like: {dayInfo.feelslike}</p>
             </div>
             <div>
                 <h2>{}</h2>
