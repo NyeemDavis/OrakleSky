b06b5481ce8586eba71689b5a01498dded04f195
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-08 07:07:31 -0400

    remaned to utils

--- oraklesky/src/config.js
+++
@@ -1,7 +0,0 @@
-// export function getDateFromString (str) {
-//     const [date,time] = str.split(" ");
-//     // reformat string into YYYY-MM-DDTHH:mm:ss.sssZ
-//     str = `${date}T${time}.000Z`
-//     return new Date(str);
-// };
-
