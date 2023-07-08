73282388e6cf2883f151bcc4a9ee10c6ec3f516d
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-08 07:06:56 -0400

    import formate date, more info added

--- oraklesky/src/day.jsx
+++ oraklesky/src/day.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {formatDate} from './utils.js';
 
 
 // Componenet to display the weather for a single day
@@ -8,11 +9,14 @@ const Day = ({dayInfo}) => {
     return (
         <div className="day-container"> 
             <div>
-                <h1>{date}</h1>
+                <h2>{formatDate(date)}</h2>
             </div>
             <div className='temps'>
                 <p>Temp: {dayInfo.temp}</p>
                 <p>Feels Like: {dayInfo.feelslike}</p>
+
+                <p>High: {dayInfo.tempmax}</p>
+                <p>Low: {dayInfo.tempmin}</p>
             </div>
             <div>
                 <h2>{}</h2>
