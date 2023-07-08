5d7136797e305cdf079c014f01933dfc108122b2
Nyeem Davis <nyeemdavis4@gmail.com>
2023-07-08 05:14:01 -0400

    Added comments and conditional rendering

--- oraklesky/src/App.js
+++ oraklesky/src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
+import dummyData from './dummydata.js'
 
 import './App.css';
 import Day from './day'
+import ErrorBox from './ErrorBox'
 
-const API_KEY = 'TWELUJNTMBUCSSD92YR6WB8QE'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 function App() {
+
+  // Hooks
+    // useSate
+      // State to hold weather data
   const [weatherData, setWeatherData] = useState({
     city: '',
     days: [],
     timezone: ''
   })
+      // State to hold current city
   const [city,  setCity] = useState('')
+      // State to hold error message
+  const [error, setError] = useState(false)
+    // useEffect
+      // useEffect to grab weather data on page load
+      useEffect(() => {{
+        console.log(dummyData.days)
+      }})
 
-
+  // Functions
+    // Async function to grab weather data from API
   const getWeatherData = async ( city ) => {
+    console.log(API_KEY)
     if(!city) {
-      alert ('Please enter a city name')
+      setError(true)
     } else {
+      setError(false)
       const API_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=${API_KEY}`
       try {
         const response = await fetch(`${API_URL}`)
@@ -28,21 +45,19 @@ function App() {
         days: data.days,
         timezone: data.timezone
       })
-      console.log(data)
       } catch (error) {
         console.log(error)
       }
-      
     }    
 }
-
-useEffect(() => {{
-  getWeatherData('London')
-}})
 
   return (
     <div className="App">
       <h1>OrakleSky</h1>
+      <div className='error-container'>
+        <ErrorBox status={error} errorMessage={'Please enter a city'}/>
+      </div>
+      
       <input 
         placeholder='Enter city name'
         value={city}
@@ -53,24 +68,43 @@ useEffect(() => {{
         Get Weather
       </button>
 
-      <div className='days'>
+      <div className='weatherInformation'>
+        <h1>
+          {/* Conditional to render address if there is active data from api or not */}
+            {
+              !weatherData.resolvedAddress 
+              ? (
+                  <h2>{dummyData.resolvedAddress}</h2>
+              ) : (
+                  <h2>{weatherData.resolvedAddress}</h2>
+              )
+            }
+          </h1>
+        <div className='days'>
+          {/* Conditional to render day information if there is data from active data from api or not */}
         {
           weatherData.days.length > 0 
           ? (
             <div className='days-container'>
-              {weatherData.days.map((day, index) => {
+              {weatherData.days.map((day) => {
                  return <Day dayInfo={day} />
               })}
             </div>
           ) : (
-            <div className='empty-container'>
-              <h1>No data</h1>
+            <div className='empty-days-container'>
+              {dummyData.days.map((day) => {
+                  return <Day dayInfo={day} />
+              })}
             </div>
           )
         }
       </div>
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
+
